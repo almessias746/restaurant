ffa73747624e413c72b8c8bbebac492d4bf77c19
Andre Messias <almessias746@gmail.com>
2020-05-19 10:52:00 -0300

    versao final

--- src/main/java/com/sioux/andre/messias/restaurant/repository/PublicRepository.java
+++ src/main/java/com/sioux/andre/messias/restaurant/repository/PublicRepository.java
@@ -1,5 +1,15 @@
 package com.sioux.andre.messias.restaurant.repository;
 
+import com.sioux.andre.messias.restaurant.domain.RestaurantDomain;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class PublicRepository {
+
+    public static RestaurantDomain findRestaurant(String name) {
+        RestaurantDomain domain = RestaurantDomain.builder().restaurant(name).build();
+        return domain;
+    }
 
 }
--- src/main/java/com/sioux/andre/messias/restaurant/service/PublicService.java
+++ src/main/java/com/sioux/andre/messias/restaurant/service/PublicService.java
@@ -1,8 +1,11 @@
 package com.sioux.andre.messias.restaurant.service;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import com.sioux.andre.messias.restaurant.domain.RestaurantDomain;
 import com.sioux.andre.messias.restaurant.dto.RestaurantDTO;
+import com.sioux.andre.messias.restaurant.dto.mapper.RestaurantMapper;
 import com.sioux.andre.messias.restaurant.repository.PublicRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,7 +18,11 @@ public class PublicService {
  PublicRepository publicRepository;
 
  public List<RestaurantDTO> findRestaurant(String name){
-  return null;
+  List<RestaurantDTO> dtos = new ArrayList<RestaurantDTO>();
+  RestaurantMapper mapper = new RestaurantMapper();
+  RestaurantDomain domain = this.publicRepository.findRestaurant(name);
+  dtos.add(mapper.toRepresentationModel(domain));
+  return dtos;
  }
 
 
